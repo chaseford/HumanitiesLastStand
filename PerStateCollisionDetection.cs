3b93f31c813cc29a3583e6ccf122a3200b807403
chaseford <chaseford@u.boisestate.edu>
2019-12-16 15:27:45 -0700

    Add files via upload

---
+++ PerStateCollisionDetection.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsefulCodeSnippet : MonoBehaviour
+{
+    //Prior to revamping group game, our concept was dodgeball
+    //The following code was as script written by Julian Do in which the dodgell causes damage to the player
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //If collider (ball) hits the player and the recieving player is not trying to catch, the player takes damage (death)
+        if (collision.collider.tag == "Player")
+        {
+            //Logs player and ball collision
+            Debug.Log("Player Collision");
+            PlayerManager pm = collision.gameObject.GetComponent<PlayerManager>();
+            //If than statement determining players catching mode
+            if (pm.IsCatching == false)
+            {
+                //On collision player is destoyed
+                Debug.Log("DEATH!");
+                pm.Death();
+                DestroyBall();
+            }
+            //After throwing ball
+            if (pm.shoot == false)
+            {
+                DestroyBall();
+            }
+            //Adjust accordingly, whether removing certain amount of health, or by destroyin gameobject on collision
+            pm.TakeDamage(damageAmount);
+        }
+    }
+
+    //Destroys prefar after throw
+    void DestroyBall()
+    {
+        Destroy(gameObject);
+    }
+}
+
+//This script can take place in any throwing/recieving games or even adjusted to be used in a shooter
+//Concept behind adjusting the damage based on which state (idle/catching) player is in can be very useful
